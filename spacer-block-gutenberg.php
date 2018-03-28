73dfb8db8a52341125df0ef24f1f0cee1c03cdff
Richard Tabor <hello@themebeans.com>
2018-03-28 11:43:00 -0400

    📦 NEW: v1.1 — Complete rewrite with create-guten-block

--- spacer-block-gutenberg.php
+++ spacer-block-gutenberg.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: Gutenberg Spacer Block Gutenberg by GutenKit
+ * Plugin Name: Gutenberg Spacer Block by GutenKit
  * Plugin URI: https://gutenkit.com
  * Description: Easily add vertical spacers to space out your blocks within the upcoming Gutenberg editor. <strong>This is a beta release.</strong>
  * Author: @@pkg.author
@@ -11,49 +11,32 @@
  * Requires at least: @@pkg.requires
  * Tested up to: @@pkg.tested_up_to
  *
- * The following was made possible in part by the Gutenberg Boilerplate
- * Check it out - https://github.com/ahmadawais/Gutenberg-Boilerplate
- *
- * GutenKit is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * any later version.
- *
- * GutenKit is distributed in the hope that it will be useful,
+ * Writy Companion is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with GutenKit. If not, see <http://www.gnu.org/licenses/>.
+ * along with Writy Companion. If not, see <http://www.gnu.org/licenses/>.
  *
- * @package   @@pkg.title for Gutenberg
+ * @package   @@pkg.title
  * @author    @@pkg.author
  * @license   @@pkg.license
  */
 
 /**
- * Main GutenKit Lite Spacer Block
+ * Main Writy Companion Class
  *
  * @since 1.0.0
  */
-class Gutenkit_Lite_Spacer_Block {
+class GutenKit_Spacer_Block {
 
 	/**
 	 * This plugin's instance.
 	 *
-	 * @var Gutenkit_Lite_Spacer_Block
+	 * @var GutenKit_Spacer_Block
 	 */
 	private static $instance;
-
-	/**
-	 * Registers the plugin.
-	 */
-	public static function register() {
-		if ( null === self::$instance ) {
-			self::$instance = new Gutenkit_Lite_Spacer_Block();
-		}
-	}
 
 	/**
 	 * The base directory path (without trailing slash).
@@ -70,21 +53,36 @@ class Gutenkit_Lite_Spacer_Block {
 	private $_url;
 
 	/**
-	 * The Plugin version.
+	 * The plugin version.
 	 *
 	 * @var string $_version
 	 */
 	private $_version;
 
 	/**
+	 * The plugin's slug.
+	 *
+	 * @var string $_slug
+	 */
+	private $_slug;
+
+	/**
+	 * Registers the plugin.
+	 */
+	public static function register() {
+		if ( null === self::$instance ) {
+			self::$instance = new GutenKit_Spacer_Block();
+		}
+	}
+
+	/**
 	 * The Constructor.
 	 */
 	private function __construct() {
-
-		$this->_version = '@@pkg.version';
-		$this->_slug    = 'gutenkit-lite-spacer-block';
-		$this->_dir     = untrailingslashit( plugin_dir_path( __FILE__ ) );
+		$this->_dir     = untrailingslashit( plugin_dir_path( '/', __FILE__ ) );
 		$this->_url     = untrailingslashit( plugins_url( '/', __FILE__ ) );
+		$this->_slug    = 'gutenkit-lite-spacer';
+		$this->_version = '@@pkg.version';
 
 		add_action( 'plugins_loaded', array( $this, 'plugins_loaded' ) );
 	}
@@ -95,7 +93,8 @@ class Gutenkit_Lite_Spacer_Block {
 	 * @access public
 	 */
 	public function plugins_loaded() {
-		add_action( 'enqueue_block_editor_assets', array( $this, 'enqueue_block_editor_assets' ) );
+		add_action( 'enqueue_block_assets', array( $this, 'block_assets' ) );
+		add_action( 'enqueue_block_editor_assets', array( $this, 'editor_assets' ) );
 	}
 
 	/**
@@ -103,25 +102,41 @@ class Gutenkit_Lite_Spacer_Block {
 	 *
 	 * @access public
 	 */
-	public function enqueue_block_editor_assets() {
-
-		// Scripts.
-		wp_enqueue_script(
-			$this->_slug,
-			$this->_url . '/block/build/index.js',
-			array( 'wp-blocks', 'wp-i18n', 'wp-element' ),
-			$this->_version
-		);
+	public function block_assets() {
 
 		// Styles.
 		wp_enqueue_style(
-			$this->_slug . '-editor',
-			$this->_url . '/block/build/editor.css',
+			$this->_slug . '-frontend',
+			$this->_url . '/dist/blocks.style.build.css',
 			array( 'wp-edit-blocks' ),
 			$this->_version
 		);
 	}
 
+	/**
+	 * Enqueue block assets for use within Gutenberg.
+	 *
+	 * @access public
+	 */
+	public function editor_assets() {
+
+		// Styles.
+		wp_enqueue_style(
+			$this->_slug . '-editor',
+			$this->_url . '/dist/blocks.editor.build.css',
+			array( 'wp-edit-blocks' ),
+			$this->_version
+		);
+
+		// Scripts.
+		wp_enqueue_script(
+			$this->_slug . '-editor',
+			$this->_url . '/dist/blocks.build.js',
+			array( 'wp-blocks', 'wp-i18n', 'wp-element' ),
+			$this->_version
+		);
+
+	}
 }
 
-Gutenkit_Lite_Spacer_Block::register();
+GutenKit_Spacer_Block::register();
