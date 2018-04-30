e3c648459c99dbb4365542e611d1e8bf04df9f83
Richard Tabor <hello@themebeans.com>
2018-04-30 15:27:42 -0400

    Update gulpfile for improved build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,149 +1,186 @@
-/**
- * Gulpfile.
- * Project Configuration for gulp tasks.
- */
+// General.
+var pkg				= require('./package.json');
+var project 			= pkg.name;
+var title			= pkg.title;
 
-var pkg                     	= require('./package.json');
-var slug                    	= pkg.slug;
-var version                	= pkg.version;
-var license                	= pkg.license;
-var copyright              	= pkg.copyright;
-var author                 	= pkg.author;
-var plugin_uri              	= pkg.plugin_uri;
+// Build.
+var buildZipDestination	  = './build/';
+var buildFiles            = ['./**', '!build', '!build/**', '!node_modules/**', '!*.json', '!*.map', '!*.xml', '!gulpfile.js', '!*.sublime-project', '!*.sublime-workspace', '!*.sublime-gulp.cache', '!*.log', '!*.DS_Store', '!*.gitignore', '!TODO', '!*.git', '!*.ftppass', '!*.DS_Store', '!sftp.json', '!yarn.lock', '!*.md', '!package.lock'];
+var cleanFiles		  = ['./build/'+project+'/', './build/'+project+' 2/', './build/'+project+'.zip' ];
+var buildDestination	  = './build/'+project+'/';
+var buildDestinationFiles = './build/'+project+'/**/*';
 
-var buildDirectory              = 'build';
-var buildFiles      	    	= ['./**', '!.gitattributes', '!node_modules/**', '!*.sublime-project', '!gulpfile.js', '!*.json', '!*.map', '!*.eslintignore', '!*.editorconfig', '!*.md', '!*.sublime-workspace', '!*.sublime-gulp.cache', '!*.log', '!CONTRIBUTING.md', '!*.DS_Store','!*.gitignore', '!*.git' ];
-var buildDestination        	= './' + buildDirectory + '/' + slug + '/';
-var buildDirectoryFiles        	= './' + buildDirectory + '/' + slug + '/**/*';
+// Release.
+var sftpDemoFilesToUpload = ['./build/' + project + '/**/*', '!/build/' + project + '/src/', '!build/' + project + '/src/**/*'];
 
 /**
  * Load Plugins.
  */
-var gulp         = require('gulp');
-var concat       = require('gulp-concat');
-var cleaner      = require('gulp-clean');
-var exec         = require('gulp-exec');
-var minifycss    = require('gulp-uglifycss');
-var rename       = require('gulp-rename');
-var sort         = require('gulp-sort');
-var notify       = require('gulp-notify');
-var copy         = require('gulp-copy');
-var replace      = require('gulp-replace-task');
-var csscomb      = require('gulp-csscomb');
-var runSequence  = require('run-sequence');
-var cache        = require('gulp-cache');
-var uglify       = require('gulp-uglify');
-var wpPot        = require('gulp-wp-pot');
-var zip          = require('gulp-zip');
-var run          = require('gulp-run-command').default;
+var gulp		= require('gulp');
+var del                 = require('del');
+var notify	   	= require('gulp-notify');
+var replace	  	= require('gulp-replace-task');
+var zip		  	= require('gulp-zip');
+var copy		= require('gulp-copy');
+var cache               = require('gulp-cache');
+var run                 = require('gulp-run-command').default;
+var sftp                = require("gulp-sftp");
+var open                = require("gulp-open");
+var gulpif              = require('gulp-if');
 
 /**
  * Tasks.
  */
-gulp.task('clear', function () {
+gulp.task('clearCache', function(done) {
 	cache.clearAll();
-} );
+	done();
+});
 
-gulp.task( 'clean', function () {
-	return gulp.src( [ './' + buildDirectory + '/*' ] , { read: false } )
-	.pipe( cleaner() );
-} );
+gulp.task('clean', function(done) {
+	return del( cleanFiles );
+	done();
+});
 
-gulp.task( 'npm-build', run( 'npm run build' ) )
+gulp.task( 'npmStart', run( 'npm run start' ) )
 
-gulp.task( 'copy', function() {
-    return gulp.src( buildFiles )
-    .pipe( copy( buildDestination ) );
-} );
+gulp.task( 'npmBuild', run( 'npm run build' ) )
 
-gulp.task( 'translate', function () {
+gulp.task( 'npmInstall', run( 'npm install' ) )
 
-	gulp.src( buildFiles )
+gulp.task('copy', function(done) {
+	return gulp.src( buildFiles )
+	.pipe( copy( buildDestination ) );
+	done();
+});
 
-	.pipe( sort() )
+gulp.task( 'updateVersion', function(done) {
+	return gulp.src( './*.php' )
 
-	.pipe( wpPot( {
-		domain        : '@@textdomain',
-		destFile      : slug + '.pot',
-		package       : pkg.title,
-		bugReport     : pkg.author_uri,
-		lastTranslator: pkg.author,
-		team          : pkg.author_shop
-	} ) )
-
-	.pipe( gulp.dest( './languages' ) )
-
-} );
-
-gulp.task( 'variables', function () {
-	return gulp.src( buildDirectoryFiles )
 	.pipe( replace( {
 		patterns: [
+			{
+				match: /(\d+\.+\d+\.+\d)/,
+				replacement: pkg.version
+			},
+		],
+		usePrefix: false
+	} ) )
+	.pipe( gulp.dest( './' ) );
+	done();
+});
+
+gulp.task('variables', function(done) {
+	return gulp.src( buildDestinationFiles )
+	.pipe(replace({
+		patterns: [
 		{
-			match: 'pkg.version',
-			replacement: version
-		},
-		{
-			match: 'textdomain',
-			replacement: slug
+			match: 'pkg.name',
+			replacement: project
 		},
 		{
 			match: 'pkg.title',
 			replacement: pkg.title
 		},
 		{
-			match: 'pkg.slug',
-			replacement: slug
-		},
-		{
-			match: 'pkg.license',
-			replacement: pkg.license
-		},
-		{
-			match: 'pkg.plugin_uri',
-			replacement: pkg.plugin_uri
+			match: 'pkg.version',
+			replacement: pkg.version
 		},
 		{
 			match: 'pkg.author',
 			replacement: pkg.author
 		},
 		{
-			match: 'pkg.author_uri',
-			replacement: pkg.author_uri
+			match: 'pkg.license',
+			replacement: pkg.license
 		},
 		{
-			match: 'pkg.requires',
-			replacement: pkg.requires
+			match: 'pkg.copyright',
+			replacement: pkg.copyright
+		},
+		{
+			match: 'textdomain',
+			replacement: pkg.name
+		},
+		{
+			match: 'pkg.downloadid',
+			replacement: pkg.downloadid
+		},
+		{
+			match: 'pkg.description',
+			replacement: pkg.description
 		},
 		{
 			match: 'pkg.tested_up_to',
 			replacement: pkg.tested_up_to
 		}
 		]
-	} ) )
+	}))
+	.pipe(gulp.dest( buildDestination ));
+	done();
+});
 
-	.pipe( gulp.dest( buildDestination ) );
-} );
+gulp.task('zip', function(done) {
+	return gulp.src( buildDestination + '/**', { base: 'build' } )
+	.pipe( zip( project + '.zip' ) )
+	.pipe( gulp.dest( buildZipDestination ) );
+	done();
+});
 
-gulp.task( 'zip', function() {
-    return gulp.src( buildDestination + '/**', { base: buildDirectory } )
-    .pipe( zip( slug +'.zip' ) )
-    .pipe( gulp.dest( './' + buildDirectory ) );
-} );
+gulp.task('build-notice', function(done) {
+	return gulp.src( './' )
+	.pipe( notify( { message: 'Your build of ' + title + ' is complete.', onLast: false } ) );
+	done();
+});
 
-gulp.task( 'clean-after-zip', function () {
-	return gulp.src( [ buildDestination, '!/' + buildDirectory + slug + '.zip'] , { read: false } )
-	.pipe(cleaner());
-} );
+gulp.task('build-process', gulp.series( 'clearCache', 'clean', 'npmBuild', 'updateVersion', 'copy', 'variables', 'zip',  function(done) {
+	done();
+} ) );
 
-gulp.task( 'finished', function () {
-	return gulp.src( '' )
-	.pipe( notify( { message: 'Your build of the ' + pkg.title + ' is complete!', onLast: false } ) );
-} );
+gulp.task('build', gulp.series( 'build-process', 'build-notice', function(done) {
+	done();
+} ) );
 
-/**
- * Build Command.
- */
-gulp.task( 'build', function( callback ) {
-	runSequence( 'clear', 'clean', 'translate', 'npm-build', 'copy', 'variables', 'zip', 'clean-after-zip', 'finished', callback );
-} );
+gulp.task( 'release-notice', function(done) {
+
+	var sftpFile;
+
+	try {
+		var sftpFile = require('./sftp.json');
+	} catch (error) {
+		done();
+	}
+
+	if (sftpFile) {
+		return gulp.src( './' )
+		.pipe( notify( { message: 'The v' + pkg.version + ' release of ' + title + ' has been uploaded.', onLast: false } ) )
+		done();
+	} else {
+		return gulp.src( './' )
+		.pipe( notify( { message: 'The release was built, but not uploaded. You do not have proper permissions to do so.', onLast: true } ) )
+		done();
+	}
+
+	done();
+});
+
+gulp.task(
+	'default',
+	gulp.series(
+		'npmStart', function(done) {
+	done();
+} ) );
+
+gulp.task(
+	'install',
+	gulp.series(
+		'npmInstall', function(done) {
+	done();
+} ) );
+
+gulp.task(
+	'release',
+	gulp.series(
+		'build-process',
+		'release-notice', function(done) {
+	done();
+} ) );
